3333b4b7839fefc032c4a584a7e2af97893fc6b9
adg1126 <gutierrez.almiradyan@gmail.com>
2024-03-22 12:15:33 +0800

    Added redux, reselect and material-tailwind to packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,23 @@
+import { useEffect } from 'react';
+import {
+  fetchProducts,
+  selectProductsArr,
+  selectProductsArrStatus,
+} from './redux/productsSlice';
+import { useDispatch, useSelector } from 'react-redux';
+
 function App() {
+  const dispatch = useDispatch();
+  const productsArr = useSelector(selectProductsArr),
+    productsArrStatus = useSelector(selectProductsArrStatus);
+
+  useEffect(() => {
+    if (productsArrStatus === 'idle') {
+      dispatch(fetchProducts());
+    }
+    console.log(productsArr);
+  }, [productsArrStatus, dispatch, productsArr]);
+
   return <></>;
 }
 
